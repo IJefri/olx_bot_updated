13339dd4a1be8a9c44c7e0938cc6640769e3baf4
Anton Yakymenko <46454906+IJefri@users.noreply.github.com>
2025-07-30 13:00:08 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,12 +10,14 @@ import requests
 from io import BytesIO
 import psutil
 import gc
+import requests
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
 
 import telebot
 
