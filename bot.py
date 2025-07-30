84f8ca1dc36bd31410e399f469b40e7c334306e8
Anton Yakymenko <46454906+IJefri@users.noreply.github.com>
2025-07-30 13:29:37 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,11 +12,6 @@ import psutil
 import gc
 import requests
 
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 from PIL import Image
 
