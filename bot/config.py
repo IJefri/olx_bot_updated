dbdbf11d8baea05e3ace3d75c9b2d5e8ea077101
IJefri <fynjy2001124@gmail.com>
2026-01-04 13:20:58 +0200

    Changed to Kyiv

--- bot/config.py
+++ bot/config.py
@@ -6,4 +6,4 @@ load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 DATABASE_URL = os.getenv("DATABASE_URL")
-OLX_BASE_URL = "https://www.olx.ua/d/uk/nedvizhimost/kvartiry/"
+OLX_BASE_URL = "https://www.olx.ua/uk/nedvizhimost/kvartiry/kiev/"
