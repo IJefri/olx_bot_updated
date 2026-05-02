ce3fd170da3201b6307f55b57dff3e909b6f2b8b
Anton Yakymenko <46454906+IJefri@users.noreply.github.com>
2026-05-02 12:26:51 +0300

    Add new chat ID environment variables

--- bot/config.py
+++ bot/config.py
@@ -5,5 +5,7 @@ load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
+CHAT_ID_15_20K = os.getenv("CHAT_ID_15_20K")
+CHAT_ID_20_25K = os.getenv("CHAT_ID_20_25K")
 DATABASE_URL = os.getenv("DATABASE_URL")
 OLX_BASE_URL = "https://www.olx.ua/uk/nedvizhimost/kvartiry/kiev/"
